b0f36c0e2f14422bd2740ce8a78a3a391c635dfe
kalle <kalle@kodapan.se>
2014-09-18 19:39:51 +0200

    0.0.7

--- app/src/androidTest/java/insamlingsappen/postnummeruppror/nu/insamlingsappen/ApplicationTest.java
+++ app/src/androidTest/java/insamlingsappen/postnummeruppror/nu/insamlingsappen/ApplicationTest.java
@@ -4,7 +4,9 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 import nu.postnummeruppror.insamlingsappen.DataStore;
+import nu.postnummeruppror.insamlingsappen.domain.Coordinate;
 import nu.postnummeruppror.insamlingsappen.domain.LocationSample;
+import nu.postnummeruppror.insamlingsappen.domain.PostalAddress;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -27,17 +29,21 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
       locationSample.setTimestampPublished(null);
       locationSample.setTimestampCreated(1l);
 
-      locationSample.setLatitude(2d);
-      locationSample.setLongitude(3d);
-      locationSample.setAltitude(4d);
-      locationSample.setAccuracy(5d);
-      locationSample.setProvider("6");
+      locationSample.setCoordinate(new Coordinate());
 
-      locationSample.setHouseNumber("7");
-      locationSample.setHouseName("8");
-      locationSample.setStreetName("9");
-      locationSample.setPostalCode("10");
-      locationSample.setPostalTown("11");
+      locationSample.getCoordinate().setLatitude(2d);
+      locationSample.getCoordinate().setLongitude(3d);
+      locationSample.getCoordinate().setAltitude(4d);
+      locationSample.getCoordinate().setAccuracy(5d);
+      locationSample.getCoordinate().setProvider("6");
+
+      locationSample.setPostalAddress(new PostalAddress());
+
+      locationSample.getPostalAddress().setHouseNumber("7");
+      locationSample.getPostalAddress().setHouseName("8");
+      locationSample.getPostalAddress().setStreetName("9");
+      locationSample.getPostalAddress().setPostalCode("10");
+      locationSample.getPostalAddress().setPostalTown("11");
 
       dataStore.create(locationSample);
 
@@ -52,16 +58,17 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
 
       assertEquals(null, locationSample.getTimestampPublished());
       assertEquals(new Long(1l), locationSample.getTimestampCreated());
-      assertEquals(new Double(2d), locationSample.getLatitude());
-      assertEquals(new Double(3d), locationSample.getLongitude());
-      assertEquals(new Double(4d), locationSample.getAltitude());
-      assertEquals(new Double(5d), locationSample.getAccuracy());
-      assertEquals("6", locationSample.getProvider());
-      assertEquals("7", locationSample.getHouseNumber());
-      assertEquals("8", locationSample.getHouseName());
-      assertEquals("9", locationSample.getStreetName());
-      assertEquals("10", locationSample.getPostalCode());
-      assertEquals("11", locationSample.getPostalTown());
+      assertEquals(new Double(2d), locationSample.getCoordinate().getLatitude());
+      assertEquals(new Double(3d), locationSample.getCoordinate().getLongitude());
+      assertEquals(new Double(4d), locationSample.getCoordinate().getAltitude());
+      assertEquals(new Double(5d), locationSample.getCoordinate().getAccuracy());
+      assertEquals("6", locationSample.getCoordinate().getProvider());
+
+      assertEquals("7", locationSample.getPostalAddress().getHouseNumber());
+      assertEquals("8", locationSample.getPostalAddress().getHouseName());
+      assertEquals("9", locationSample.getPostalAddress().getStreetName());
+      assertEquals("10", locationSample.getPostalAddress().getPostalCode());
+      assertEquals("11", locationSample.getPostalAddress().getPostalTown());
 
 
       locationSample.setServerIdentity(12l);
@@ -79,16 +86,17 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
       assertEquals(new Long(12l), locationSample.getServerIdentity());
       assertEquals(new Long(13l), locationSample.getTimestampPublished());
       assertEquals(new Long(1l), locationSample.getTimestampCreated());
-      assertEquals(new Double(2d), locationSample.getLatitude());
-      assertEquals(new Double(3d), locationSample.getLongitude());
-      assertEquals(new Double(4d), locationSample.getAltitude());
-      assertEquals(new Double(5d), locationSample.getAccuracy());
-      assertEquals("6", locationSample.getProvider());
-      assertEquals("7", locationSample.getHouseNumber());
-      assertEquals("8", locationSample.getHouseName());
-      assertEquals("9", locationSample.getStreetName());
-      assertEquals("10", locationSample.getPostalCode());
-      assertEquals("11", locationSample.getPostalTown());
+      assertEquals(new Double(2d), locationSample.getCoordinate().getLatitude());
+      assertEquals(new Double(3d), locationSample.getCoordinate().getLongitude());
+      assertEquals(new Double(4d), locationSample.getCoordinate().getAltitude());
+      assertEquals(new Double(5d), locationSample.getCoordinate().getAccuracy());
+      assertEquals("6", locationSample.getCoordinate().getProvider());
+
+      assertEquals("7", locationSample.getPostalAddress().getHouseNumber());
+      assertEquals("8", locationSample.getPostalAddress().getHouseName());
+      assertEquals("9", locationSample.getPostalAddress().getStreetName());
+      assertEquals("10", locationSample.getPostalAddress().getPostalCode());
+      assertEquals("11", locationSample.getPostalAddress().getPostalTown());
 
 
     } finally {
